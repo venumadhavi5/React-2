1e1d696d09b54588f0720aa9c05f3b73725b10a0
venumadhavi5 <venumadhavi3350@gmail.com>
2024-09-17 09:38:03 +0530

    Add files via upload

---
+++ react-2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><ins>Lord Krishna</ins></h1>
+      <img width="500px" height="500px" src="https://m.media-amazon.com/images/I/71yc4G74FZL.jpg"></img>
+      <p>Lord Krishna, the eighth avatar of Lord Vishnu, possessed 16 kalas or divine qualities. These qualities include compassion, patience, forgiveness, justice, impartiality, detachment, spiritual powers, invincibility, generosity, beauty, dance, singing, honesty, truth, mastery of all arts, and control</p>
+    </div>
+  );
+}
+
+export default App;
